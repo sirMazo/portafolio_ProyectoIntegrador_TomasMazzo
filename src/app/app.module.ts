82feaa2ccf34bed71705e328300bec4d47be5e6c
Tomás Mazzo <116767949+sirMazo@users.noreply.github.com>
2023-01-18 18:53:03 -0300

    Primeros pasos en el FrontEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/header/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/header/social/social.component';
+import { BannerComponent } from './components/header/banner/banner.component';
+import { LoginComponent } from './components/header/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
