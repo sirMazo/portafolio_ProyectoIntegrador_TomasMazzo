d538a035f7b561492094fe3e4cba7909227a02a3
Tomás Mazzo <116767949+sirMazo@users.noreply.github.com>
2023-01-24 17:35:19 -0300

    inicio terminado, mejorando el navbar | FRONTEND

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/header/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/header/social/social.component';
 import { LoginComponent } from './components/header/login/login.component';
 import { AcercaDeComponent } from './components/header/acerca-de/acerca-de.component';
 import { NavLinksComponent } from './components/header/nav-links/nav-links.component';
@@ -16,7 +15,6 @@ import { InicioComponent } from './components/header/inicio/inicio.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent,
     LoginComponent,
     AcercaDeComponent,
     NavLinksComponent,
