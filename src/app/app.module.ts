df09dee83b105d3c819a7758f29992680872aaff
Tomás Mazzo <116767949+sirMazo@users.noreply.github.com>
2023-02-04 19:40:23 -0300

    Responsive and Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/header/logo-ap/logo-ap.component';
 import { LoginComponent } from './components/header/login/login.component';
-import { AcercaDeComponent } from './components/header/acerca-de/acerca-de.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { NavLinksComponent } from './components/header/nav-links/nav-links.component';
-import { InicioComponent } from './components/header/inicio/inicio.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
